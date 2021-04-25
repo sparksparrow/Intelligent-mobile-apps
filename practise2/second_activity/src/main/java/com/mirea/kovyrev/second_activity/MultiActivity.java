17e6a6df9e0aa23d6641f5848bde20bf79a74165
AlekseiKovyrev <50149745+sparksparrow@users.noreply.github.com>
2021-04-25 21:04:29 +0300

    Add files via upload

---
+++ practise2/second_activity/src/main/java/com/mirea/kovyrev/second_activity/MultiActivity.java
@@ -0,0 +1,22 @@
+package com.mirea.kovyrev.second_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MultiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi);
+    }
+
+    public void onClickNewActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("key", "MIREA - ФАМИЛИЯ ИМЯ ОТЧЕТСВО СТУДЕНТА");
+        startActivity(intent);
+    }
+}
