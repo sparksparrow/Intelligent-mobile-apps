b37a3a8c61cef024c9a4102f352932f3aec6a072
AlekseiKovyrev <50149745+sparksparrow@users.noreply.github.com>
2021-04-25 21:06:10 +0300

    Add files via upload

---
+++ practise2/app/src/main/java/com/mirea/kovyrev/practise2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.mirea.kovyrev.practise2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, LifecycleActivity.class);
+        startActivity(intent);
+    }
+}
